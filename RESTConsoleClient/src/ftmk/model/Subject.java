b813001d688dd4c8392ee17eac8d6e6ce7989ee1
Afiqah Halim <fiqa0105@gmail.com>
2021-07-01 23:55:54 +0800

    Initial Commit

---
+++ RESTConsoleClient/src/ftmk/model/Subject.java
@@ -0,0 +1,27 @@
+package ftmk.model;
+
+/*
+ * By fiqa 
+ */
+
+public class Subject {
+	
+	private int subjectId;
+	private String name;
+	
+	public int getSubjectId() {
+		return subjectId;
+	}
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
